afd4302c51c1af02af72ef530d2d88e086978e0d
Josh Morrow <josh@jcmorrow.com>
2019-08-02 21:10:17 -0400

    Draw an arrow on the screen

--- src/index.js
+++ src/index.js
@@ -1,45 +1,60 @@
+function preload() {
+}
+
+function create() {
+    this.state = { pointer: {} };
+    this.planets = [
+        {
+            x: 300, y: 300, radius: 100
+        }
+    ]
+
+    this.graphics = this.add.graphics(0, 0);
+    this.input.on('pointermove', function (pointer) {
+
+        let v = new Phaser.Math.Vector2(pointer.worldX, pointer.worldY);
+        this.state.pointer = { x: v.x, y: v.y };
+
+    }, this);
+}
+
+function update() {
+    this.graphics.destroy();
+    this.graphics = this.add.graphics(0, 0);
+
+
+    this.planets.forEach((p) => {
+        this.matter.add.circle(p.x, p.y, p.radius);
+        this.graphics.fillStyle(0xFFFFFF, 1.0);
+        this.graphics.fillCircle(p.x, p.y, p.radius);
+    });
+
+    this.graphics.lineStyle(5, 0xFF00FF, 1.0);
+    this.graphics.beginPath();
+    this.graphics.moveTo(this.planets[0].x, this.planets[0].y);
+    this.graphics.lineTo(
+        (this.state.pointer.x - this.planets[0].x) * 100,
+        (this.state.pointer.y - this.planets[0].y) * 100
+    );
+    this.graphics.closePath();
+    this.graphics.strokePath();
+};
+
 var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
     physics: {
-        default: 'arcade',
+        default: 'matter',
         arcade: {
             gravity: { y: 200 }
         }
     },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update,
     }
 };
 
 var game = new Phaser.Game(config);
-
-function preload() {
-    this.load.setBaseURL('http://labs.phaser.io');
-
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
-}
-
-function create() {
-    this.add.image(400, 300, 'sky');
-
-    var particles = this.add.particles('red');
-
-    var emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-    });
-
-    var logo = this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
-}
