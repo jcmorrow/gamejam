3269e826ef30a3dca85ad4ef8f7b11518266bbfa
Cameron McCallie <mccallie.cameron@gmail.com>
2019-08-03 15:04:29 -0400

    shphysics

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    // backgroundColor: '#FFFFFF',
     physics: {
         default: 'matter',
-        // gravity: {
-        //     // y: 0
-        //     // scale: 0
-        // },
         matter: {
             plugins: {
                 attractors: true
@@ -24,86 +19,70 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+const Matter = Phaser.Physics.Matter.Matter;
+
+function gravity(bodyA, bodyB) {
+    var bToA = Matter.Vector.sub(bodyB.position, bodyA.position),
+        distanceSq = Matter.Vector.magnitudeSquared(bToA) || 0.0001,
+        normal = Matter.Vector.normalise(bToA),
+        magnitude = -0.1 * (bodyA.mass * bodyB.mass / distanceSq),
+        force = Matter.Vector.mult(normal, magnitude);
+
+    // only apply force to our ship, which I guess is always bodyB
+    // Matter.Body.applyForce(bodyA, bodyA.position, Matter.Vector.neg(force));
+    Matter.Body.applyForce(bodyB, bodyB.position, force);
+}
 
 function preload() {
     this.load.image('planet', 'black_planet.jpeg');
 }
 
 function create() {
-    // this.state = { pointer: {}, theta: 0 };
-    // this.planets = [
-    //     {
-    //         x: 300, y: 300, radius: 50
-    //     }
-    // ]
-
-    // this.graphics = this.add.graphics(0, 0);
-    // this.input.on('pointermove', function (pointer) {
-    //
-    //     let v = new Phaser.Math.Vector2(pointer.worldX, pointer.worldY);
-    //     this.state.pointer = { x: v.x, y: v.y };
-    //
-    // }, this);
-
     this.matter.world.setBounds(0, 0, game.config.width, game.config.height);
-    // this.matter.world.engine.world.gravity.y = 0;
-    let graphics = this.add.graphics({ lineStyle: { color: 0xff00ff } });
-    let planet = new Phaser.Geom.Circle(400, 300, 50);
-    let planet2 = this.matter.add.image(400, 200, 'planet', null, {
+    this.matter.world.engine.world.gravity.y = 0;
+
+    let graphics = this.add.graphics({
+        lineStyle: { color: 0xff00ff },
+        fillStyle: { color: 0xffffff }
+    });
+
+    let planet = new Phaser.Geom.Circle(400, 300, 20);
+
+    let planet2 = this.matter.add.image(500, 100, 'planet', null, {
         shape: {
             type: 'circle',
-            radius: 35
+            radius: 60
+        },
+        plugin: {
+            attractors: [gravity]
+        },
+        mass: 100
+    });
+
+    let planet3 = this.matter.add.image(100, 500, 'planet', null, {
+        shape: {
+            type: 'circle',
+            radius: 60
+        },
+        plugin: {
+            attractors: [gravity]
+        },
+        mass: 200
+    });
+
+    planet2.body.isStatic = true;
+    planet3.body.isStatic = true;
+
+    let ship = this.matter.add.circle(200, 50, 10, {
+        plugin: {
+            attractors: [gravity]
         }
     });
 
-    // let circle = this.matter.add.circle(200, 200, 50);
-
     graphics.strokeCircleShape(planet);
-    graphics.lineStyle(1, 0x00ff00);
-    graphics.lineBetween(planet.x, planet.y, planet.x + planet.radius, planet.y);
-    graphics.strokeRect(planet.left, planet.top, planet.diameter, planet.diameter);
-
-    this.matter.add.rectangle(300, 200, 100, 100, {
-        chamfer: { radius: [90, 0, 0, 0] }
-    });
-
-
-    // this.matter.add.circle(600, 100, 50, {
-    //     plugin: {
-    //         attractors: [
-    //             MatterAttractors.Attractors.gravity
-    //         ]
-    //     }
-    // });
-    // this.matter.add.circle(600, 100, 50, {
-    //     plugin: {
-    //         attractors: [
-    //             function (bodyA, bodyB) {
-    //                 return {
-    //                     x: (bodyA.position.x - bodyB.position.x) * 0.000001,
-    //                     y: (bodyA.position.y - bodyB.position.y) * 0.000001
-    //                 }
-    //             }
-    //         ]
-    //     }
-    // });
+    graphics.fillCircleShape(planet);
 }
 
 function update() {
-    // this.graphics.destroy();
-    // this.graphics = this.add.graphics(0, 0);
 
-
-    // this.planets.forEach((p) => {
-    //     this.matter.add.circle(p.x, p.y, p.radius);
-        // this.graphics.fillStyle(0xFF0FFF, 1.0);
-        // this.graphics.fillCircle(p.x, p.y, p.radius);
-    // });
-
-    // this.graphics.fillStyle(0x00FFFF, 1.0);
-    // let shipX = this.planets[0].x + Math.cos(this.state.theta) * 180;
-    // let shipY = this.planets[0].y + Math.sin(this.state.theta) * 120;
-
-    // this.graphics.fillCircle(shipX+60, shipY-20, 10);
-    // this.state.theta = this.state.theta + 0.04;
 };
